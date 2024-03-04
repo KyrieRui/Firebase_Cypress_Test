284752e31d98bc8e7aa20fafafee848a847d5273
KyrieRui <kyrie19960324@gmail.com>
2024-03-04 13:55:00 +1300

    UI design for logsign page

--- log-sign/src/Components/log-sign/logsign.jsx
+++ log-sign/src/Components/log-sign/logsign.jsx
@@ -14,31 +14,26 @@ const LogSign = () => {
       </div>
       <div className="inputs">
         <div className="input">
-          <img
-            src={user_icon}
-            alt=""
-            style={{ width: "25px", height: "25px" }}
-          />
+          <img src={user_icon} alt="" />
           <input type="text" placeholder="Name" />
         </div>
 
         <div className="input">
-          <img
-            src={email_icon}
-            alt=""
-            style={{ width: "25px", height: "25px" }}
-          />
-          <input type="email" placeholder="Name" />
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email" />
         </div>
 
         <div className="input">
-          <img
-            src={passowrd_icon}
-            alt=""
-            style={{ width: "25px", height: "25px" }}
-          />
-          <input type="password" placeholder="Name" />
+          <img src={passowrd_icon} alt="" />
+          <input type="password" placeholder="Password" />
         </div>
+      </div>
+      <div className="forgot-password">
+        Can't remember password?<span>Click here</span>
+      </div>
+      <div className="submitt-container">
+        <div className="submit">Sign up</div>
+        <div className="submit">Log in</div>
       </div>
     </div>
   );
