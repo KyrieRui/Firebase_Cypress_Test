107091dbd418c2793555ba50286c9ef358d04da1
KyrieRui <kyrie19960324@gmail.com>
2024-03-05 18:28:31 +1300

    Firebase link demo

---
+++ react-firebase-auth/src/components/auth/SignIn.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        console.log(userCredential);
+        // ...
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="sign-in-container">
+      <form onSubmit={signIn}>
+        <h1>Log In</h1>
+        <input
+          type="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
