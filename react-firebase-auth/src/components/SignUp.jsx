6ec2d458119a3dd9304f04944447a1e9b21829f1
KyrieRui <kyrie19960324@gmail.com>
2024-03-06 21:49:36 +1300

    Create SignUp.jsx

---
+++ react-firebase-auth/src/components/SignUp.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        console.log(userCredential);
+        // ...
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="sign-in-container">
+      <form onSubmit={signUp}>
+        <h1>Log In</h1>
+        <input
+          type="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
