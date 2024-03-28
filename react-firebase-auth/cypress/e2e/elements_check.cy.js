8ee52c600178a395277c32b59890c797533c3780
KyrieRui <kyrie19960324@gmail.com>
2024-03-29 10:47:08 +1300

    Create elements_check.cy.js

---
+++ react-firebase-auth/cypress/e2e/elements_check.cy.js
@@ -0,0 +1,16 @@
+describe("SignIn Component", () => {
+  it("should input email and password", () => {
+    cy.visit("http://localhost:3000/"); // local host page
+
+    // check if the input fields are visible
+    cy.get('input[type="email"]').eq(0).should("be.visible");
+    cy.get('input[type="password"]').eq(0).should("be.visible");
+
+    // check if the input fields are empty
+    cy.get('input[type="email"]').eq(0).should("have.value", "");
+    cy.get('input[type="password"]').eq(0).should("have.value", "");
+
+    // check if the Log In button is visible
+    cy.get('button[type="submit"]').eq(0).should("be.visible");
+  });
+});
